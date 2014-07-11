e258903c2c8c52c219ae51fcec9744f45c98c2d3
toshi <toshi@pixiv.com>
2014-07-11 15:49:26 +0900

    updated

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -175,6 +175,6 @@
 ;; package.el ;;
 ;;;;;;;;;;;;;;;;
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
